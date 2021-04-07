520b34cf9bbdd5226b14d0826c31b015085b1c54
StefanoCeli <stefanopsn91@gmail.com>
2021-04-07 16:07:46 +0200

    ottimizzato codice

--- js/script.js
+++ js/script.js
@@ -8,17 +8,14 @@ if(isNaN(eta_utente)){
 }
 var tariffa = 0.21;
 var costo = km_utente * tariffa;
-//var percentuale = costo / 100;
-//var under = percentuale *20
-//var over = percentuale *40
-var under = (costo / 100) * 20;
-var over = (costo / 100)* 40;
+var under = costo - ((costo / 100) * 20);
+var over = costo - ((costo / 100) * 40);
 
 if(eta_utente <18){
-    document.getElementById('biglietto').innerHTML = "Il prezzo del biglietto è di: " + costo.toFixed(2)  + "€" +" per te riservato con sconto del 20% a: " + (costo - under).toFixed(2) + "€";
+    document.getElementById('biglietto').innerHTML = "Il prezzo del biglietto è di: " + costo.toFixed(2)  + "€" +" per te riservato con sconto del 20% a: " + under.toFixed(2) + "€";
 }
 else if (eta_utente >65){
-    document.getElementById('biglietto').innerHTML = "Il prezzo del biglietto è di: " + costo.toFixed(2)  + "€"+" per te riservato con sconto del 40% a: " + (costo - over).toFixed(2) + "€";
+    document.getElementById('biglietto').innerHTML = "Il prezzo del biglietto è di: " + costo.toFixed(2)  + "€"+" per te riservato con sconto del 40% a: " + over.toFixed(2) + "€";
 }
 else{
     document.getElementById('biglietto').innerHTML = "Il prezzo del biglietto è di: " + costo.toFixed(2)  + "€";
