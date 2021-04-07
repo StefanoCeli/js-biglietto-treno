34e99ab4a5299e4dccc219fe9d6cf3d0018d82a0
StefanoCeli <stefanopsn91@gmail.com>
2021-04-07 15:32:02 +0200

    Riordinato codice correttamente

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-var tariffa = 0.21;
-var costo = km_utente * tariffa;
-var percentuale = costo / 100;
-var under = percentuale * 20;
-var over = percentuale * 40;
-
 var km_utente = parseInt(prompt('Inserisci i km da percorrere'));
 if(isNaN(km_utente)){
     alert('inserisci un numero');
@@ -12,6 +6,11 @@ var eta_utente = parseInt(prompt('Inserisci la tua età'));
 if(isNaN(eta_utente)){
     alert('inserisci un numero');
 }
+var tariffa = 0.21;
+var costo = km_utente * tariffa;
+var percentuale = costo / 100;
+var under = percentuale * 20;
+var over = percentuale * 40;
 
 if(eta_utente <18){
     document.getElementById('biglietto').innerHTML = "Il prezzo del biglietto è di: " + costo.toFixed(2)  + "€" +" per te riservato con sconto del 20% a: " + (costo - under).toFixed(2) + "€";
